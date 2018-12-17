f02c7423d7df2a6ddca1d168dbd91b1c05877af1
Danu Kumanan <danu3006@gmail.com>
2018-12-17 11:52:19 +0000

    More abstraction work on Keystore, Config and Data Directory

--- dist/tools/index.d.ts
+++
@@ -1,8 +0,0 @@
-import Config from "./classes/Config";
-import Keystore from "./classes/Keystore";
-export default class DataDirectory {
-    readonly path: string;
-    readonly config: Config;
-    readonly keystore: Keystore;
-    constructor(path: string);
-}
