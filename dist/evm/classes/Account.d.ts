260f3414f26416057b68022f45e694dfa692e4d9
Danu Kumanan <danu3006@gmail.com>
2019-01-07 15:20:37 +0000

    Clean up

--- dist/evm/classes/Account.d.ts
+++ dist/evm/classes/Account.d.ts
@@ -2,13 +2,13 @@ import { Account as Web3Account, V3JSONKeyStore } from 'web3-eth-accounts';
 import { BaseAccount } from '../..';
 import Transaction, { SignedTransaction, TX } from './Transaction';
 export default class Account {
-    readonly address: string;
-    readonly privateKey: string;
-    static decrypt(v3JSONKeyStore: V3JSONKeyStore, password: string): Account;
     balance: number;
     nonce: number;
     private readonly account;
     constructor(data?: Web3Account);
+    readonly address: string;
+    readonly privateKey: string;
+    static decrypt(v3JSONKeyStore: V3JSONKeyStore, password: string): Account;
     sign(message: string): any;
     signTransaction(tx: TX | Transaction): Promise<SignedTransaction>;
     encrypt(password: string): V3JSONKeyStore;
