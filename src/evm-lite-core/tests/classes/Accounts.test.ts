c8493567ba94fa6b9727d777b1a9a24311195b69
Danu Kumanan <danu3006@gmail.com>
2019-02-06 12:18:10 +0000

    Fixes tests

--- src/evm-lite-core/tests/classes/Accounts.test.ts
+++ src/evm-lite-core/tests/classes/Accounts.test.ts
@@ -1,61 +1,56 @@
 import evmlc from '../setup';
 
-describe('Accounts.ts', () => {
-	describe('constructor', () => {
-		const defaultFrom = evmlc.defaultFrom;
+import { Transaction } from '../../src';
 
-		it('should default gas from EVMLC', () => {
-			expect(evmlc.accounts.defaults.gas).toBe(evmlc.defaultGas);
-		});
+const defaultFrom = evmlc.defaultFrom;
+let transaction: Transaction;
+let parsed: any;
 
-		it('should default gasPrice from EVMLC', () => {
-			expect(evmlc.accounts.defaults.gasPrice).toBe(
-				evmlc.defaultGasPrice
-			);
-		});
-
-		it('should default from from EVMLC', () => {
-			expect(evmlc.accounts.defaults.from).toBe(evmlc.defaultFrom);
-		});
-
-		it('should have the default gas from `EVMLC` after change', () => {
-			evmlc.defaultFrom = 'ASD';
-			evmlc.defaultGas = 9999;
-			evmlc.defaultGasPrice = 12;
-
-			expect(evmlc.accounts.defaults.gas).toBe(9999);
-		});
-
-		it('should have the default gasPrice from `EVMLC` after change', () => {
-			expect(evmlc.accounts.defaults.gasPrice).toBe(12);
-		});
-
-		it('should have the default `from` from `EVMLC` after change', () => {
-			expect(evmlc.accounts.defaults.from).toBe('ASD');
-
-			evmlc.defaultFrom = defaultFrom;
-			evmlc.defaultGas = 1000000;
-			evmlc.defaultGasPrice = 0;
-		});
+describe('Accounts.ts', async () => {
+	it('should default gas from EVMLC', async () => {
+		expect(evmlc.accounts.defaults.gas).toBe(evmlc.defaultGas);
 	});
 
-	describe('prepareTransfer()', async () => {
-		const transaction = await evmlc.accounts.prepareTransfer(
-			'SOME_ADDRESS',
-			200
-		);
-		const parsed = transaction.parse();
+	it('should default gasPrice from EVMLC', () => {
+		expect(evmlc.accounts.defaults.gasPrice).toBe(evmlc.defaultGasPrice);
+	});
 
-		it('transaction has default gas', async () => {
-			expect(parsed.gas).toBe(evmlc.defaultGas);
-		});
+	it('should default from from EVMLC', () => {
+		expect(evmlc.accounts.defaults.from).toBe(evmlc.defaultFrom);
+	});
 
-		it('transaction has default gasPrice', async () => {
-			expect(parsed.gasPrice).toBe(evmlc.defaultGasPrice);
-		});
+	it('should have the default gas from `EVMLC` after change', () => {
+		evmlc.defaultFrom = 'ASD';
+		evmlc.defaultGas = 9999;
+		evmlc.defaultGasPrice = 12;
 
-		it('transaction has default from', async () => {
-			expect(parsed.from).toBe(evmlc.defaultFrom.toLowerCase());
-		});
+		expect(evmlc.accounts.defaults.gas).toBe(9999);
+	});
+
+	it('should have the default gasPrice from `EVMLC` after change', () => {
+		expect(evmlc.accounts.defaults.gasPrice).toBe(12);
+	});
+
+	it('should have the default `from` from `EVMLC` after change', () => {
+		expect(evmlc.accounts.defaults.from).toBe('ASD');
+
+		evmlc.defaultFrom = defaultFrom;
+		evmlc.defaultGas = 1000000000;
+		evmlc.defaultGasPrice = 0;
+	});
+
+	it('transaction has default gas', async () => {
+		transaction = await evmlc.accounts.prepareTransfer('SOME_ADDRESS', 200);
+		parsed = transaction.parse();
+
+		expect(parsed.gas).toBe(evmlc.accounts.defaultGas);
+	});
+
+	it('transaction has default gasPrice', async () => {
+		expect(parsed.gasPrice).toBe(evmlc.accounts.defaultGasPrice);
+	});
+
+	it('transaction has default from', async () => {
+		expect(parsed.from).toBe(evmlc.accounts.defaultFrom.toLowerCase());
 	});
 });
--- src/evm-lite-core/tests/classes/Contracts.test.ts
+++ src/evm-lite-core/tests/classes/Contracts.test.ts
@@ -1,57 +1,53 @@
 import evmlc from '../setup';
 
+let contract: any;
+
 describe('Contracts.ts', () => {
-	describe('constructor', () => {
-		const defaultFrom = evmlc.defaultFrom;
+	const defaultFrom = evmlc.defaultFrom;
 
-		it('should have the default gas from `EVMLC`', () => {
-			expect(evmlc.contracts.defaults.gas).toBe(evmlc.defaultGas);
-		});
-
-		it('should have the default gasPrice from `EVMLC`', () => {
-			expect(evmlc.contracts.defaults.gasPrice).toBe(
-				evmlc.defaultGasPrice
-			);
-		});
-
-		it('should have the default `from` from `EVMLC`', () => {
-			expect(evmlc.contracts.defaults.from).toBe(evmlc.defaultFrom);
-		});
-
-		it('should have the default gas from `EVMLC` after change', () => {
-			evmlc.defaultFrom = 'ASD';
-			evmlc.defaultGas = 9999;
-			evmlc.defaultGasPrice = 12;
-
-			expect(evmlc.contracts.defaults.gas).toBe(9999);
-		});
-
-		it('should have the default gasPrice from `EVMLC` after change', () => {
-			expect(evmlc.contracts.defaults.gasPrice).toBe(12);
-		});
-
-		it('should have the default `from` from `EVMLC` after change', () => {
-			expect(evmlc.contracts.defaults.from).toBe('ASD');
-
-			evmlc.defaultFrom = defaultFrom;
-			evmlc.defaultGas = 1000000;
-			evmlc.defaultGasPrice = 0;
-		});
+	it('should have the default gas from `EVMLC`', () => {
+		expect(evmlc.contracts.defaults.gas).toBe(evmlc.defaultGas);
 	});
 
-	describe('load()', async () => {
-		const contract = await evmlc.contracts.load([]);
+	it('should have the default gasPrice from `EVMLC`', () => {
+		expect(evmlc.contracts.defaults.gasPrice).toBe(evmlc.defaultGasPrice);
+	});
 
-		it('contract should have default gas', async () => {
-			expect(contract.options.gas).toBe(evmlc.defaultGas);
-		});
+	it('should have the default `from` from `EVMLC`', () => {
+		expect(evmlc.contracts.defaults.from).toBe(evmlc.defaultFrom);
+	});
 
-		it('contract should have default gasPrice', async () => {
-			expect(contract.options.gasPrice).toBe(evmlc.defaultGasPrice);
-		});
+	it('should have the default gas from `EVMLC` after change', () => {
+		evmlc.defaultFrom = 'ASD';
+		evmlc.defaultGas = 9999;
+		evmlc.defaultGasPrice = 12;
 
-		it('contract should have default from', async () => {
-			expect(contract.options.from.value).toBe(evmlc.defaultFrom);
-		});
+		expect(evmlc.contracts.defaults.gas).toBe(9999);
+	});
+
+	it('should have the default gasPrice from `EVMLC` after change', () => {
+		expect(evmlc.contracts.defaults.gasPrice).toBe(12);
+	});
+
+	it('should have the default `from` from `EVMLC` after change', () => {
+		expect(evmlc.contracts.defaults.from).toBe('ASD');
+
+		evmlc.defaultFrom = defaultFrom;
+		evmlc.defaultGas = 1000000;
+		evmlc.defaultGasPrice = 0;
+	});
+
+	it('contract should have default gas', async () => {
+		contract = await evmlc.contracts.load([]);
+
+		expect(contract.options.gas).toBe(evmlc.defaultGas);
+	});
+
+	it('contract should have default gasPrice', async () => {
+		expect(contract.options.gasPrice).toBe(evmlc.defaultGasPrice);
+	});
+
+	it('contract should have default from', async () => {
+		expect(contract.options.from.value).toBe(evmlc.defaultFrom);
 	});
 });
