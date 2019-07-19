e6de760da664597bdc7c3df22f580df8c4c2b13a
Danu <danu3006@gmail.com>
2019-07-19 17:08:06 +0100

    update: Changed default config file name to `wallet.toml` Also removed duplicated functions from `utils``

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -12,35 +12,6 @@ export default class Utils {
 		return `0x${address}`;
 	}
 
-	public static isEquivalentObjects(objectA: any, objectB: any) {
-		const aProps = Object.getOwnPropertyNames(objectA);
-		const bProps = Object.getOwnPropertyNames(objectB);
-
-		if (aProps.length !== bProps.length) {
-			return false;
-		}
-
-		for (const propName of aProps) {
-			if (
-				typeof objectA[propName] === 'object' &&
-				typeof objectB[propName] === 'object'
-			) {
-				if (
-					!Utils.isEquivalentObjects(
-						objectA[propName],
-						objectB[propName]
-					)
-				) {
-					return false;
-				}
-			} else if (objectA[propName] !== objectB[propName]) {
-				return false;
-			}
-		}
-
-		return true;
-	}
-
 	public static trimHex(hex: string) {
 		hex = hex.toLowerCase();
 
