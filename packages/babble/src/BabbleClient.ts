fc139089b4d85a4575d7fa84fcc2297c5ddcf6a5
Danu <danu3006@gmail.com>
2019-08-08 16:41:22 +0100

    update: Initial set up for Babble module

---
+++ packages/babble/src/BabbleClient.ts
@@ -0,0 +1,35 @@
+import * as JSONBig from 'json-bigint';
+
+import AbstractClient from 'evm-lite-core';
+
+// Babble block interface
+export interface BabbleBlock {
+	Body: {
+		Index: number;
+		RoundReceived: number;
+		StateHash: string;
+		FrameHash: string;
+		PeersHash: string;
+		Transactions: string[];
+		InternalTransactions: any[];
+		InternalTransactionReceipts: any[];
+	};
+	Signatures: {
+		[key: string]: string;
+	};
+}
+
+class BabbleClient extends AbstractClient {
+	constructor(host: string, port: number) {
+		super(host, port);
+	}
+
+	// methods here
+	public getBlock(index: number): Promise<BabbleBlock> {
+		return this.get(`/block/${index}`).then(
+			(response: string) => JSONBig.parse(response) as BabbleBlock
+		);
+	}
+}
+
+export default BabbleClient;
