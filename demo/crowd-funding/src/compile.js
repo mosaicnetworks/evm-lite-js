7c76a258fe07029aeccb9be49224ccdd59354627
Danu <danu3006@gmail.com>
2019-08-28 16:49:27 +0100

    update: Crowd-funding demo

--- demo/crowd-funding/src/compile.js
+++ demo/crowd-funding/src/compile.js
@@ -1,33 +1,33 @@
-const fs = require("fs");
+const fs = require('fs');
 
 // solc@0.5.10
-const solc = require("solc");
+const solc = require('solc');
 
 // compile solidity contract
 function compile(contractPath, contractName) {
-  const input = {
-    language: "Solidity",
-    sources: {
-      Contracts: {
-        content: fs.readFileSync(contractPath, "utf8")
-      }
-    },
-    settings: {
-      outputSelection: {
-        "*": {
-          "*": ["*"]
-        }
-      }
-    }
-  };
+	const input = {
+		language: 'Solidity',
+		sources: {
+			Contracts: {
+				content: fs.readFileSync(contractPath, 'utf8')
+			}
+		},
+		settings: {
+			outputSelection: {
+				'*': {
+					'*': ['*']
+				}
+			}
+		}
+	};
 
-  const output = JSON.parse(solc.compile(JSON.stringify(input)));
-  const compiledContract = output.contracts.Contracts[`${contractName}`];
+	const output = JSON.parse(solc.compile(JSON.stringify(input)));
+	const compiledContract = output.contracts.Contracts[`${contractName}`];
 
-  return {
-    bytecode: compiledContract.evm.bytecode.object,
-    abi: compiledContract.abi
-  };
+	return {
+		bytecode: compiledContract.evm.bytecode.object,
+		abi: compiledContract.abi
+	};
 }
 
 module.exports = compile;
--- demo/getting-started.md
+++ demo/getting-started.md
@@ -47,7 +47,7 @@ This will install all `evm-lite-js` modules as well as the `Solidity` compiler `
 Now that we have a running node (`Monet` or `EVM-Lite`), we will need to verify that the account we will be dealing with has enough funds. We can use the CLI to speed up the process of this by running
 
 ```bash
-$ evmlc accounts list -f
+$ evmlc accounts list
 
 .-----------------------------------------------------------------------------.
 |                  Address                   |        Balance         | Nonce |
@@ -55,8 +55,6 @@ $ evmlc accounts list -f
 | 0x07BA865451D9417714E8Bb89e715ACBc789A1Bb7 | 1234000000000000000000 |     0 |
 '-----------------------------------------------------------------------------'
 ```
-
-The `-f, --formatted` flag specifies the output to be formatted in a table rather than raw `json`.
 
 ## Setting Up Contract
 
